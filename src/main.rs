9dc4c0895199d64c239dc67bf175caee8b4d6434
Poyotoron <poyo@poyopoyo>
2024-02-22 18:00:05 +0900

    コントローラーの接続を認識できた

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use windows::Gaming::Input::{self, Gamepad};
+use windows::Gaming::Input::{self, Gamepad, RawGameController};
 
 fn main() {
     println!("Hello, world!");
 
-    for gamepad in Gamepad::Gamepads() {
-        println!("Gamepad: {:?}", gamepad);
+    // 箱コン以外はRawGameControllerで読むっぽい
+    while let Ok(gamepad) = RawGameController::RawGameControllers() {
+        println!("RawGameController: {:?}", gamepad); // なにこれ
+        println!("RawGameController Size: {:?}", gamepad.Size()); // 接続中のコントローラーの台数
+
+        // let reading = Gamepad::GetCurrentReading(&Gamepad);
     }
 }
